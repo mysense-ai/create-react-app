eb2d5c3918c28f48552209c254d41cc971c61709
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2019-07-21 22:47:53 +0100

    fix: update eslint config frontend

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -320,7 +320,10 @@ module.exports = function(webpackEnv) {
                 eslintPath: require.resolve('eslint'),
                 // @remove-on-eject-begin
                 baseConfig: {
-                  extends: [require.resolve('@mysense/eslint-config-frontend')],
+                  extends: [
+                    require.resolve('eslint-config-react-app'),
+                    require.resolve('@mysense/eslint-config-frontend'),
+                  ],
                 },
                 ignore: false,
                 useEslintrc: false,
